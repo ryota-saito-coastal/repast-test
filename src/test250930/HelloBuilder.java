0f1c15b5f77f621688b2daca241d0fd4528d1cff
Ryota Saito <118103331+ryota-saito-coastal@users.noreply.github.com>
2025-10-01 08:36:48 +0900

    Fix context initialization for Hello model

--- src/test250930/HelloBuilder.java
+++ src/test250930/HelloBuilder.java
@@ -3,7 +3,10 @@ package test250930;
 import repast.simphony.context.Context;
 import repast.simphony.context.DefaultContext;
 import repast.simphony.dataLoader.ContextBuilder;
+import repast.simphony.engine.environment.DefaultControllerAction;
 import repast.simphony.engine.environment.RunEnvironmentBuilder;
+import repast.simphony.engine.environment.RunState;
+import repast.simphony.parameter.Parameters;
 import repast.simphony.scenario.ModelInitializer;
 import repast.simphony.scenario.Scenario;
 
@@ -12,7 +15,7 @@ public class HelloBuilder implements ContextBuilder<Object>, ModelInitializer {
     @Override
     public Context<Object> build(Context<Object> context) {
         if (context == null) {
-            context = new DefaultContext<>("HelloContext");
+            context = new DefaultContext<>("test250930");
         }
         for (int i = 0; i < 5; i++) {
             context.add(new HelloAgent(i));
@@ -22,6 +25,12 @@ public class HelloBuilder implements ContextBuilder<Object>, ModelInitializer {
 
     @Override
     public void initialize(Scenario scen, RunEnvironmentBuilder builder) {
-        // ここでは何もせず、ContextBuilder の build に任せる
+        scen.addMasterControllerAction(new DefaultControllerAction() {
+            @Override
+            public void runInitialize(RunState runState, Object contextId, Parameters runParams) {
+                Context<Object> context = build(new DefaultContext<>("test250930"));
+                runState.setMasterContext(context);
+            }
+        });
     }
 }
