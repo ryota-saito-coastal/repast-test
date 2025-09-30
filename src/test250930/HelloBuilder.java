ecf7f5bf404df4ee00cba72eca2d0e56b6aecbb8
Ryota Saito <saitoh-r@p.mpat.go.jp>
2025-09-30 18:53:36 +0900

    as

--- src/test250930/HelloBuilder.java
+++ src/test250930/HelloBuilder.java
@@ -1,16 +1,27 @@
 package test250930;
 
 import repast.simphony.context.Context;
+import repast.simphony.context.DefaultContext;
 import repast.simphony.dataLoader.ContextBuilder;
+import repast.simphony.engine.environment.RunEnvironmentBuilder;
+import repast.simphony.scenario.ModelInitializer;
+import repast.simphony.scenario.Scenario;
 
-public class HelloBuilder implements ContextBuilder<Object> {
+public class HelloBuilder implements ContextBuilder<Object>, ModelInitializer {
 
     @Override
     public Context<Object> build(Context<Object> context) {
-        context.setId("HelloContext");
+        if (context == null) {
+            context = new DefaultContext<>("HelloContext");
+        }
         for (int i = 0; i < 5; i++) {
             context.add(new HelloAgent(i));
         }
         return context;
+    }
+
+    @Override
+    public void initialize(Scenario scen, RunEnvironmentBuilder builder) {
+        // ここでは何もせず、ContextBuilder の build に任せる
     }
 }
