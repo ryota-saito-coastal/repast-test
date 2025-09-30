df471a6d33fa322679e0b617a7dd6f67211c7ab9
Ryota Saito <saitoh-r@p.mpat.go.jp>
2025-10-01 08:56:39 +0900

    modified:   bin/test250930/HelloBuilder.class modified:   debug.log modified:   src/test250930/HelloBuilder.java modified:   test250930.rs/context.xml modified:   test250930.rs/scenario.xml

--- src/test250930/HelloBuilder.java
+++ src/test250930/HelloBuilder.java
@@ -3,34 +3,17 @@ package test250930;
 import repast.simphony.context.Context;
 import repast.simphony.context.DefaultContext;
 import repast.simphony.dataLoader.ContextBuilder;
-import repast.simphony.engine.environment.DefaultControllerAction;
-import repast.simphony.engine.environment.RunEnvironmentBuilder;
-import repast.simphony.engine.environment.RunState;
-import repast.simphony.parameter.Parameters;
-import repast.simphony.scenario.ModelInitializer;
-import repast.simphony.scenario.Scenario;
 
-public class HelloBuilder implements ContextBuilder<Object>, ModelInitializer {
+public class HelloBuilder implements ContextBuilder<Object> {
 
     @Override
     public Context<Object> build(Context<Object> context) {
         if (context == null) {
-            context = new DefaultContext<>("test250930");
+            context = new DefaultContext<>("test250930"); // ← context ID
         }
         for (int i = 0; i < 5; i++) {
             context.add(new HelloAgent(i));
         }
         return context;
-    }
-
-    @Override
-    public void initialize(Scenario scen, RunEnvironmentBuilder builder) {
-        scen.addMasterControllerAction(new DefaultControllerAction() {
-            @Override
-            public void runInitialize(RunState runState, Object contextId, Parameters runParams) {
-                Context<Object> context = build(new DefaultContext<>("test250930"));
-                runState.setMasterContext(context);
-            }
-        });
     }
 }
